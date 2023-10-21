1967a2ae94b88f04c68020a83b0e1472518eef33
AnkitAdhikari <ankitadhikari31512@gmail.com>
2023-10-21 10:25:58 +0545

    Basic Hello World

---
+++ 01-basic-challenges-1/01-hello-world/hello-world.js
@@ -0,0 +1,11 @@
+// # Challenge: Hello World Sample Challenge
+
+// This is a practice challenge to show you how things are set up and how to test, etc.
+
+// ## Instructions
+
+// Write a function called `helloWorld` that returns a string of 'Hello World!'.
+
+function helloWorld() {
+  return 'Hello World!';
+}
