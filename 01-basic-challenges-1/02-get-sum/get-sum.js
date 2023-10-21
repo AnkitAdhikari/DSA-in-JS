38bae4cc2297d6d4739f3db48914fe34e5966df1
AnkitAdhikari <ankitadhikari31512@gmail.com>
2023-10-21 10:31:18 +0545

    add getSum challenge solution

---
+++ 01-basic-challenges-1/02-get-sum/get-sum.js
@@ -0,0 +1,10 @@
+// # Challenge: Get Sum
+
+
+// ## Instructions
+
+// Write a function called `getSum` that takes in two numbers and returns the sum of those two numbers.
+
+function getSum(x, y) {
+  return x + y;
+}
