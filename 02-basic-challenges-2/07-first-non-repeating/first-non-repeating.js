75ce3bdf60b2781e380a92eb1ae18b6d4cc3c723
AnkitAdhikari <ankitadhikari31512@gmail.com>
2023-11-08 11:19:41 +0545

    updated first non repeating with performant code

--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,12 +1,34 @@
+// function findFirstNonRepeatingCharacter(str) {
+//   for (let i = 0; i < str.length; i++) {
+//     let count = 0;
+//     for (let j = 0; j < str.length; j++) {
+//       if (str[i] === str[j]) count++
+//     }
+//     if (count === 1) {
+//       return str[i];
+//     }
+//   }
+//   return null;
+// }
+
+
+
+// more performant solutin
+
 function findFirstNonRepeatingCharacter(str) {
-  for (let i = 0; i < str.length; i++) {
-    let count = 0;
-    for (let j = 0; j < str.length; j++) {
-      if (str[i] === str[j]) count++
-    }
-    if (count === 1) {
-      return str[i];
+  const charCount = new Map();
+
+  for (char of str) {
+    charCount.set(char, (charCount.get(char) || 0) + 1);
+  }
+
+
+  for (char of str) {
+    if (charCount.get(char) === 1) {
+      return char;
     }
   }
+
   return null;
+
 }
