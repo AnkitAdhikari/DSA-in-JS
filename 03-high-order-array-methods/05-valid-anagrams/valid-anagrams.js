6bb8cc91446df1399bf9e2fea35a20219f8753bd
AnkitAdhikari <ankitadhikari31512@gmail.com>
2023-11-16 13:27:09 +0545

    add missed instruction

--- 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
+++ 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
@@ -1,3 +1,9 @@
+// # Challenge: Valid Anagrams
+
+// ## Instructions
+
+// Write a function called `validAnagrams` that takes in two strings and determines whether they are valid anagrams of each other. An anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
 function validAnagrams(str1, str2) {
   if (str1.length !== str2.length) {
     return false;
--- 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
+++ 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
@@ -1,3 +1,15 @@
+// # Challenge: Hashtag Generator
+
+// ## Instructions
+
+// Write a function called `generateHashtag` that takes a string as input and returns a hashtag-generated string according to the rules below. If the generated hashtag string is longer than 140 characters or the input/result is an empty string, the function should return `false`.
+// ### Constraints
+
+// - Return `false` if the input string is empty or contains only whitespace characters.
+// - Return `false` if the generated hashtag string is longer than 140 characters.
+// - Every word in the hashtag should start with a capital letter.
+// - The input string may contain leading/trailing whitespace characters.
+
 function generateHashtag(str) {
   const trimmedStr = str.trim();
   if (trimmedStr.length > 140 || trimmedStr.length === 0) return false;
